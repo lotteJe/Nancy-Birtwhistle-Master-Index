788035fd3225e8585e7556b39194cdf6e93d8d87
Lotte Jespers <lotje.j@hotmail.com>
2026-03-31 17:33:11 +0200

    title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -186,7 +186,7 @@ export default function App() {
               {isSearching ? (
                 <div className="w-5 h-5 border-2 border-white/30 border-t-white rounded-full animate-spin" />
               ) : (
-                <Sparkles className="w-5 h-5" />
+                <Search className="w-5 h-5" />
               )}
               Search
             </button>
